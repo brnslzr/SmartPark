c6a7abfec3fb4c731d7a3e9abee24a484912abd6
Brian Salazar <brianallynsalazar@gmail.com>
2026-03-31 22:35:30 +0800

    Initial commit project setup

---
+++ src/test/java/com/SmartPark/smartpark_api/SmartparkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartPark.smartpark_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartparkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
