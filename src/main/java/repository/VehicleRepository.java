b10301f3254b8b960cbdfe3e450ea721a5b05fc7
Brian Salazar <brianallynsalazar@gmail.com>
2026-04-01 00:31:07 +0800

    Change repository package name

--- src/main/java/repository/VehicleRepository.java
+++ src/main/java/repository/VehicleRepository.java
@@ -1,6 +1,5 @@
-package Repository;
+package repository;
 
-import model.ParkingRecord;
 import model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
